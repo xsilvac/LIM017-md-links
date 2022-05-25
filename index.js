fa3252d35e931448c392e44da8bcd28dd32247fa
ximena silva <ximenasilvacotrina11@gmail.com>
2022-05-24 23:22:31 -0500

    Nuevas funciones de nodeJs. Close #3

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ export const isRouteExists = route => fs.existsSync(route);
 
 //FUNCION QUE CONVIERTE A RUTA ABSOLUTA
 export const convertAbsolutePath = route => path.resolve(route);
+
+//FUNCION VERIFICAR SI LA RUTA ES UNA CARPETA
+export const isADirectory = route => fs.statSync(route).isDirectory();
+
+//FUNCION QUE LEE ARCHIVOS
+export const readFile = route => fs.readFileSync(route, 'utf8', (err, data) => {
+    if (err) throw err;
+    return (data);
+})
+
+//FUNCION QUE VERIFICA SI ES UN ARCHIVO .MD
+export const isFileMd = route => path.extname(route) === '.md';
+//console.log(isFileMd('C:\Users\Jimena\Downloads\laboratoria\LIM017-md-links\src\Archivos\prueba.md'))
+
+const rutasEncotradas = ruta => fs.readdirSync(ruta, 'utf8');
+
+export const unirRuta = route => rutasEncotradas(route).map(elemento => path.join(route, elemento));
+//console.log(unirRuta('src/Archivos'))
+
