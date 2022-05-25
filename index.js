71b59faaa6f0459e3696e3ea07a46ca06ba3a5f5
ximena silva <ximenasilvacotrina11@gmail.com>
2022-05-24 23:17:19 -0500

    Verficar si la ruta existe y modificar el  package. Close #1

--- index.js
+++ index.js
@@ -4,5 +4,3 @@ import path from 'path';
 //FUNCION VERIFICAR SI LA RUTA EXISTE
 export const isRouteExists = route => fs.existsSync(route);
 
-//FUNCION QUE CONVIERTE A RUTA ABSOLUTA
-export const convertAbsolutePath = route => path.resolve(route);
